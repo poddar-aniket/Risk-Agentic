0f3e8577dad8d4828e27cda191ad13dd1940dfee
Aishvary Dwivedi <aishvarydwivedi2006@gmail.com>
2026-06-19 10:07:14 +0530

    Revert "Feature/1 event extraction"

--- app/utils/logger.py
+++
@@ -1,9 +0,0 @@
-"""Basic logging setup, called once at app startup."""
-import logging
-
-
-def setup_logging(level: str = "INFO") -> None:
-    logging.basicConfig(
-        level=getattr(logging, level.upper(), logging.INFO),
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    )
